5269f213bf9794bc1b3723642ed928122e91eff5
RobSheffield <rearle1@sheffield.ac.uk>
2026-04-10 15:22:52 +0100

    update to .sh file for hpc

--- hpc_train.sh
+++ hpc_train.sh
@@ -1,36 +1,26 @@
 #!/bin/bash
-#SBATCH --job-name=xray-train
-#SBATCH --output=training_%j.log
-#SBATCH --error=training_%j.err
-#SBATCH --time=04:00:00
+#SBATCH --job-name=K-flips_test
+#SBATCH --output=training_Kflips2_%j.log
+#SBATCH --error=training_Kflips2_%j.err
+#SBATCH --time=15:00:00
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
-#SBATCH --mem=32G
+#SBATCH --mem=36G
 #SBATCH --cpus-per-task=4
-
 module load Python/3.11.3-GCCcore-12.3.0
 module load CUDA/12.4.0
 module load OpenBLAS/0.3.23-GCC-12.3.0
 
 
 cd /users/acb22re/Dissertation
-python -m venv .venv
+#python -m venv .venv
 source .venv/bin/activate
-pip install --upgrade pip
-pip install -r hpc_requirements.txt
+
+#pip install --upgrade pip
+#pip install -r hpc_requirements.txt
 
 
 #source /users/acb22re/CopiedDissertation/take2/X-Ray_Image_Analysis/.venv/bin/activate
 
-python -m helpers.train_terminal \
-  --data_yaml "data/dataset_yaml/dataset.yaml" \
-  --model_info '{"name":"hpc_model_flips","model":"YOLOv5","number_of_images":"3000","date_time_trained":"","total_training_time":"","path":"","epoch":"100","box_loss":"","cls_loss":"","mAP_50":"","mAP_50_95":"","precision":"","recall":"","dataset_config":"All Images","starting_model":"","folder_name":"","metamorphic_test_result":"","differential_test_result":"","fuzzing_test_result":""}' \
-  --model_dir "trained_models/hpc_model_flips" \
-  --weights yolov5mu.pt \
-  --img_size 1280 \
-  --batch_size 12 \
-  --epochs 50 \
-  --flips True
-
-
+python helpers/k_fold_leakage_test.py
 echo "Training completed!"
