3b96cc4dcc93ee3edf8608f6919aba35c0a859ce
RobSheffield <rearle1@sheffield.ac.uk>
2026-04-10 15:16:40 +0100

    k_fold hopefully distinguishes final results for bias

---
+++ helpers/k_fold_leakage_test.py
@@ -0,0 +1,14 @@
+import k_fold
+
+
+if __name__ == "__main__":
+    k=10
+    k_fold.create_bias_folds("Castings", "Bias_folds", k, testSize=0.1,seed=42)
+    k_fold.build_train_val_sets("Bias_folds")
+    k_fold.train_all("Bias_folds","biased_models")
+    k_fold.mAP_on_test_set("Bias_folds/test","biased_models")   
+
+    k_fold.create_folds("Castings", "Folds", k, testSize=0.1,seed=42)
+    k_fold.build_train_val_sets("Folds")
+    k_fold.train_all("Folds","unbiased_models")
+    k_fold.mAP_on_test_set("Folds/test","unbiased_models")
