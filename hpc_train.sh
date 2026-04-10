cfb0e18056309d0574a20f584ad5ae3de8344d0b
RobSheffield <rearle1@sheffield.ac.uk>
2026-04-10 15:32:37 +0100

    longer SLRUM time

--- hpc_train.sh
+++ hpc_train.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=K-flips_test
-#SBATCH --output=training_Kflips2_%j.log
-#SBATCH --error=training_Kflips2_%j.err
-#SBATCH --time=15:00:00
+#SBATCH --output=training_bias2_%j.log
+#SBATCH --error=training_bias2_%j.err
+#SBATCH --time=30:00:00
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
-#SBATCH --mem=36G
+#SBATCH --mem=32G
 #SBATCH --cpus-per-task=4
 module load Python/3.11.3-GCCcore-12.3.0
 module load CUDA/12.4.0
