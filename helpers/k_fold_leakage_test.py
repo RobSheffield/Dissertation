3a4c0e88b24e54131ffe614ca5f84088d259ac6c
RobSheffield <rearle1@sheffield.ac.uk>
2026-04-11 14:33:28 +0100

    failed training, just redoing unbiased

--- helpers/k_fold_leakage_test.py
+++ helpers/k_fold_leakage_test.py
@@ -5,11 +5,6 @@ import k_fold
 if __name__ == "__main__":
     k=10
     device = os.getenv("K_FOLD_DEVICE", "auto")
-    k_fold.create_bias_folds("Castings", "Bias_folds", k, testSize=0.1,seed=42)
-    k_fold.build_train_val_sets("Bias_folds")
-    k_fold.train_all("Bias_folds","biased_models", device=device)
-    k_fold.mAP_on_test_set("Bias_folds/test","biased_models")   
-
     k_fold.create_folds("Castings", "Folds", k, testSize=0.1,seed=42)
     k_fold.build_train_val_sets("Folds")
     k_fold.train_all("Folds","unbiased_models", device=device)
