bd58d16445e33913ce88ea35ae81ab71b0da473e
RobSheffield <rearle1@sheffield.ac.uk>
2026-04-14 18:40:22 +0100

    only changes wanted to keep

--- helpers/k_fold_leakage_test.py
+++ helpers/k_fold_leakage_test.py
@@ -5,7 +5,11 @@ import k_fold
 if __name__ == "__main__":
     k=10
     device = os.getenv("K_FOLD_DEVICE", "auto")
-    k_fold.create_folds("Castings", "Folds", k, testSize=0.1,seed=42)
-    k_fold.build_train_val_sets("Folds")
-    k_fold.train_all("Folds","unbiased_models", device=device)
-    k_fold.mAP_on_test_set("Folds/test","unbiased_models")
+    '''    k_fold.create_bias_folds("Castings", "Bias_folds", k, testSize=0.1,seed=42)
+    k_fold.build_train_val_sets("Bias_folds")
+    k_fold.train_all("Bias_folds","biased_models", device=device)
+    k_fold.mAP_on_test_set("Bias_folds/test","biased_models")   '''
+    k_fold.create_folds("Castings", "Folds_strat_5", k=k, testSize=0.2, seed=42)
+    k_fold.build_train_val_sets("Folds_strat_5")
+    k_fold.train_all("Folds_strat_5", "strat_5_unbiased_models", device=device)
+    k_fold.mAP_on_test_set("Folds_strat_5/test", "strat_5_unbiased_models")
--- helpers/metamorphic_retraining_analysis.py
+++ helpers/metamorphic_retraining_analysis.py
@@ -3,8 +3,11 @@ import k_fold
 
 
 if __name__ == "__main__":
-    k_fold.create_folds("Castings", "Folds_augmented", 10, testSize=0,seed=42,augment=True)
-    k_fold.create_folds("Castings", "Folds_non_augmented", 10, testSize=0,seed=42,augment=False)
+    k_fold.create_folds("Castings", "Folds_augmented", k=10, testSize=0,seed=42)
+    k_fold.create_folds("Castings", "Folds_non_augmented", k=10, testSize=0,seed=42)
+    k_fold.build_train_val_sets("Folds_non_augmented", apply_training_augmentations=False)
+    k_fold.build_train_val_sets("Folds_non_augmented", apply_training_augmentations=True)
+
     k_fold.train_all("Folds_augmented","models_augmented", device="auto")
     k_fold.train_all("Folds_non_augmented","models_non_augmented", device="auto")
     k_fold.train_all("Folds_non_augmented","models_vertical_flip", device="auto",flips=True)
