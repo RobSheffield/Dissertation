2a766d0b0f9491a58a076f9b679e83897ad5f541
RobSheffield <rearle1@sheffield.ac.uk>
2026-04-15 12:19:51 +0100

    sanity test for ignoring non.gt files

--- helpers/k_fold_leakage_test.py
+++ helpers/k_fold_leakage_test.py
@@ -13,36 +13,11 @@ def cleanup_dir(path):
 if __name__ == "__main__":
     device = os.getenv("K_FOLD_DEVICE", "auto")
 
-    # k = 5 with test split
-    try:
-        k = 5
-        folds_path = "Folds_strat_5"
-        model_path = "strat_5_unbiased_models"
-
-        k_fold.create_folds("Castings", folds_path, k=k, testSize=0.2, seed=42)
-        k_fold.build_train_val_sets(folds_path)
-        k_fold.train_all(folds_path, model_path, device=device)
-        k_fold.mAP_on_test_set(f"{folds_path}/test", model_path)
-    finally:
-        cleanup_dir(folds_path)
-
-    # k = 5 no test split
-    try:
-        k = 5
-        folds_path = "Folds_strat_5_no_test"
-        model_path = "strat_5_unbiased_models"
-
-        k_fold.create_folds("Castings", folds_path, k=k, testSize=0, seed=42)
-        k_fold.build_train_val_sets(folds_path)
-        k_fold.train_all(folds_path, model_path, device=device)
-    finally:
-        cleanup_dir(folds_path)
-
     # k = 10 with test split
     try:
         k = 10
-        folds_path = "Folds_strat_10"
-        model_path = "strat_10_unbiased_models"
+        folds_path = "Folds_strat_10_full_test"
+        model_path = "strat_10_unbiased_models_full_test"
 
         k_fold.create_folds("Castings", folds_path, k=k, testSize=0.2, seed=42)
         k_fold.build_train_val_sets(folds_path)
